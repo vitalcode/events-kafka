87061532a2fe3977495c6b3c2770dc8c342ab04e
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-04-12 21:59:09 +0100

    Evaluate nested variables

--- start-kafka.sh
+++ start-kafka.sh
@@ -5,11 +5,11 @@ do
   if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME ]]; then
     kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
-    env_var=$(eval "echo ${env_var}")
+    env_value=$(eval "echo ${!env_var}")
     if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${env_value}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
     else
-        echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+        echo "$kafka_name=${env_value}" >> $KAFKA_HOME/config/server.properties
     fi
   fi
 done
